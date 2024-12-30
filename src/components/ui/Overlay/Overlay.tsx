47bf7e7cc981d59b87bc870f12a77f7df659d014
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-12-30 15:48:58 +0200

    🔒 add close button

--- src/components/ui/Overlay/Overlay.tsx
+++ src/components/ui/Overlay/Overlay.tsx
@@ -1,7 +1,23 @@
-import type { FC } from 'react';
+import cn from 'classnames';
+import type { ComponentProps } from 'react';
 
-const Overlay: FC<unknown> = () => {
-  return <div></div>;
-};
+import { slotable } from '@/src/hoc';
+
+import {
+  type OverlayVariantsType,
+  overlayVariants,
+} from './Overlay.variants.ts';
+
+interface OverlayProps extends ComponentProps<'div'> {}
+
+const Overlay = slotable<'div', OverlayProps & OverlayVariantsType>(
+  'div',
+  ({ Comp, className, shrink, ...props }) => (
+    <Comp
+      className={cn(overlayVariants({ shrink }), className)}
+      {...props}
+    />
+  ),
+);
 
 export default Overlay;
