9cf360c433d7493332bcf5d1545027b1a88abe80
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-01-04 19:23:02 +0200

    🩼 create PermissionsGuard

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import type { FC, PropsWithChildren } from 'react';
 
 import DashboardLayout from '@/src/components/layout/DashboardLayout';
+import { PermissionsGuard } from '@/src/components/layout/PermissionsGuard';
 import { NO_INDEX_PAGE } from '@/src/constants/seo.constants.ts';
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -12,7 +13,13 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 const AdminLayout: FC<PropsWithChildren> = ({ children }) => {
-  return <DashboardLayout variant={'admin'}>{children}</DashboardLayout>;
+  return (
+    <DashboardLayout variant={'admin'}>
+      <PermissionsGuard features={['canViewDbStatus']} />
+
+      {children}
+    </DashboardLayout>
+  );
 };
 
 export default AdminLayout;
---
+++ src/components/layout/PermissionsGuard/index.ts
@@ -0,0 +1,3 @@
+import PermissionsGuard from './PermissionsGuard';
+
+export { PermissionsGuard };
