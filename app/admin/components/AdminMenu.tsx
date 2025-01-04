97b14b0731d46a35ac4951de220b1dc517e1b828
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-01-04 14:15:22 +0200

    ⌾ add loader to AdminMenu

--- app/admin/components/AdminMenu.tsx
+++ app/admin/components/AdminMenu.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import cn from 'classnames';
 import type { ComponentProps, FC } from 'react';
 
 import type DashboardMenu from '@/src/components/layout/DashboardMenu';
@@ -9,20 +10,33 @@ import {
   dashboardBottomMenuData,
 } from '@/src/components/layout/DashboardMenu/dashboard.menu.data.ts';
 import { Divider } from '@/src/components/ui';
+import CircleLoader from '@/src/components/ui/CircleLoader';
 import { useFeatures } from '@/src/hooks/useFeatures.ts';
 
 const AdminMenu: FC<Pick<ComponentProps<typeof DashboardMenu>, 'variant'>> = ({
   variant,
 }) => {
-  const { data } = useFeatures();
+  const { data, isLoading } = useFeatures();
 
   const canAccess = !!data?.canAccessAdminPage;
 
   return (
     <>
+      <Divider />
+
+      {isLoading ? (
+        <>
+          <div
+            aria-hidden
+            className={cn('aspect-square flex-center')}
+          >
+            <CircleLoader />
+          </div>
+        </>
+      ) : undefined}
+
       {canAccess ? (
         <>
-          <Divider />
           {traverseNavItems(
             variant === 'admin' ? adminBottomMenuData : dashboardBottomMenuData,
           )}
