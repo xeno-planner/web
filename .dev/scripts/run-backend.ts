cc3189360d74c58c8faeb14c2783443ef7d52f98
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-08-23 20:26:18 +0200

    ✍️ change path in script

--- .dev/scripts/run-backend.ts
+++ .dev/scripts/run-backend.ts
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
+// @ts-expect-error TL to fix types
 import sh from 'shelljs';
 
+dotenv.config();
 const exec = sh.exec;
 
-sh.cd('../backend');
+sh.cd(process.env.BACKEND_PROJECT_PATH ?? '../backend');
 
 exec('yarn');
 exec('yarn migrate:dev --name ci');
