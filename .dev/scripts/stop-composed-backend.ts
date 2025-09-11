347ccbd071f74d8f5bfadd8a343b8bab2fbfb1e5
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-11 21:08:58 +0200

    🏃 create run scripts

---
+++ .dev/scripts/stop-composed-backend.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+// @ts-expect-error TL to fix types
+import sh from 'shelljs';
+
+dotenv.config();
+const exec = sh.exec;
+
+sh.cd(process.env.BACKEND_PROJECT_PATH ?? '../backend');
+
+exec('docker compose stop');
